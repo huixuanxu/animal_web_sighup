3d7ac72a5c7cd8483a67ef382394efc1d986dcec
huixuanxu <angel911024a@gmail.com>
2025-11-30 03:24:28 +0800

    Initial commit of FastAPI Supabase Backend

---
+++ src/lib/env.js
@@ -0,0 +1,12 @@
+
+window.__ENV = {
+  VITE_FIREBASE_API_KEY: "AIzaSyALGApEjuADOOWzE9uIqXzGolrg8FNMAiY",
+  VITE_FIREBASE_AUTH_DOMAIN: "animal-website-account.firebaseapp.com",
+  VITE_FIREBASE_PROJECT_ID: "animal-website-account",
+  VITE_FIREBASE_STORAGE_BUCKET: "animal-website-account.firebasestorage.app",
+  VITE_FIREBASE_MESSAGING_SENDER_ID: "801839513580",
+  VITE_FIREBASE_APP_ID: "1:80183951358:web:539ff54a7604def9fc0aaf",
+  
+  // API URL
+  API_BASE_URL: "http://127.0.0.1:5000", // 這個變數應根據您的應用程式邏輯確認是否需要
+};
